b77bc3d8746efea9e295f0870e1d8fc6a7a927ec
manolo <mmartinortiz@gmail.com>
2023-12-31 15:13:48 +0100

    Use the RangeSlicer with the Wheel and Gear

--- code.py
+++ code.py
@@ -9,49 +9,7 @@ import usb_hid
 from hid_gamepad import Gamepad
 from analogio import AnalogIn
 from digitalio import DigitalInOut, Direction, Pull
-
-led = DigitalInOut(board.LED)
-led.direction = Direction.OUTPUT
-led.value = True
-
-print("Initializing the gamepad")
-# Create a gamepad for HID simulation
-gp = Gamepad(usb_hid.devices)
-
-# Other pins can also be set to output
-# for detecting buttons.
-out = DigitalInOut(board.GP16)
-out.direction = Direction.OUTPUT
-out.value = True
-
-buttons = {
-    i + 1: {"device": DigitalInOut(device), "pressed": False}
-    for i, device in enumerate(
-        [
-            # Buttons in the steering wheel, from 1 to 6
-            board.GP4,
-            board.GP9,
-            board.GP2,
-            board.GP3,
-            board.GP6,
-            board.GP5,
-            # Buttons in the gear lever, from 7 to 10
-            board.GP11,
-            board.GP13,
-            board.GP12,
-            board.GP7,
-        ],
-    )
-}
-
-steering_wheel = AnalogIn(board.A0)
-gear_lever = AnalogIn(board.A1)
-
-for _, button in buttons.items():
-    button["device"].direction = Direction.INPUT
-    button["device"].pull = Pull.DOWN
-
-print("Setup finished")
+import cedargrove_rangeslicer as rs
 
 
 def calibrate_analog_input(device: AnalogIn) -> None:
@@ -116,7 +74,7 @@ def get_gear_position(device: AnalogIn) -> int:
     Returns:
         int: Value between -127 and 127
     """
-    return scale_voltage(device.value, min_voltage=16720, max_voltage=50016)
+    return scale_voltage(device.value, min_voltage=17428, max_voltage=59742)
 
 
 def get_wheel_position(device: AnalogIn) -> int:
@@ -132,6 +90,87 @@ def get_wheel_position(device: AnalogIn) -> int:
 
 
 # calibrate_analog_input(steering_wheel)
+# calibrate_analog_input(gear_lever)
+
+
+"""Establish range_slicer instances, one for each analog potentiometer
+input. Input ranges are adjusted for unique potentiometer inaccuracies and
+noise. Slice size divides the output into 10 slices. Hysteresis factor is
+25% of a slice."""
+
+led = DigitalInOut(board.LED)
+led.direction = Direction.OUTPUT
+led.value = True
+
+print("Initializing the gamepad")
+# Create a gamepad for HID simulation
+gp = Gamepad(usb_hid.devices)
+
+# Other pins can also be set to output
+# for detecting buttons.
+out = DigitalInOut(board.GP16)
+out.direction = Direction.OUTPUT
+out.value = True
+
+buttons = {
+    i + 1: {"device": DigitalInOut(device), "pressed": False}
+    for i, device in enumerate(
+        [
+            # Buttons in the steering wheel, from 1 to 6
+            board.GP4,
+            board.GP9,
+            board.GP2,
+            board.GP3,
+            board.GP6,
+            board.GP5,
+            # Buttons in the gear lever, from 7 to 10
+            board.GP11,
+            board.GP13,
+            board.GP12,
+            board.GP7,
+        ],
+    )
+}
+
+steering_wheel = AnalogIn(board.A0)
+gear_lever = AnalogIn(board.A1)
+
+for _, button in buttons.items():
+    button["device"].direction = Direction.INPUT
+    button["device"].pull = Pull.DOWN
+
+print("Setup finished")
+
+
+# Set the known minimum and maximum value
+steering_min_value = 3520
+steering_max_value = 61712
+
+gear_min_value = 18428
+gear_max_value = 58742
+
+
+def new_slicer(min_value, max_value):
+    print("Slicer recreated")
+    return rs.Slicer(
+        in_min=min_value,
+        in_max=max_value,
+        out_min=-127,
+        out_max=127,
+        out_slice=1,
+        hyst_factor=0.1,
+        out_integer=True,
+    )
+
+
+steering = new_slicer(min_value=steering_min_value, max_value=steering_max_value)
+steering_needs_update = False
+
+gear = new_slicer(min_value=gear_min_value, max_value=gear_max_value)
+gear_needs_update = False
+
+debug = False
+
 while True:
     for idx, button in buttons.items():
         if not button["device"].value and button["pressed"]:
@@ -144,7 +183,42 @@ while True:
             print(f"Button {idx} pressed!")
             gp.press_buttons(idx)
 
-    gear_position = get_gear_position(gear_lever)
-    wheel_position = get_wheel_position(steering_wheel)
+    steering_wheel_value = steering_wheel.value
+    gear_value = gear_lever.value
+
+    wheel_position = steering.range_slicer(steering_wheel_value)[0]
+    gear_position = gear.range_slicer(gear_value)[0]
+
+    # Update max and min values
+    if steering_wheel_value > steering_max_value:
+        steering_max_value = steering_wheel_value
+        steering_needs_update = True
+
+    if steering_wheel_value < steering_min_value:
+        steering_min_value = steering_wheel_value
+        steering_needs_update = True
+
+    if gear_value > gear_max_value:
+        gear_max_value = gear_value
+        gear_needs_update = True
+
+    if gear_value < gear_min_value:
+        gear_min_value = gear_value
+        gear_needs_update = True
+
+    if steering_needs_update:
+        steering = new_slicer(
+            min_value=steering_min_value, max_value=steering_max_value
+        )
+        steering_needs_update = False
+
+    if gear_needs_update:
+        gear = new_slicer(min_value=gear_min_value, max_value=gear_max_value)
+        gear_needs_update = False
 
     gp.move_joysticks(x=wheel_position, y=gear_position)
+
+    if debug:
+        print(f"Wheel max: {steering_max_value} Wheel min {steering_min_value}")
+        print(f"Gear max: {gear_max_value} Gear min {gear_min_value}")
+        print(f"wheel: {wheel_position} gear: {gear_position}")
